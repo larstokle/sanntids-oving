cc240c81aa072e698e1109674c13b794805bf55b
Kari Engen Guttulsrud <eparis@atlassian.com>
2016-02-06 14:12:48 +0100

    Listener for TCP

---
+++ Ex3/basicTCPListen.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	//"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	strEcho := "Halo \000"
+	//servAddr := "129.241.187.23:33546"
+	tcpAddr, err := net.ResolveTCPAddr("tcp", ":20005")
+	if err != nil {
+		println("ResolveTCPAddr failed:", err.Error())
+		os.Exit(1)
+	}
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		println("Dial failed:", err.Error())
+		os.Exit(1)
+	}
+
+	conn, err := listener.AcceptTCP()
+
+	reply := make([]byte, 1024)
+
+	_, err = conn.Read(reply)
+	if err != nil {
+		println("Write to server failed:", err.Error())
+		os.Exit(1)
+	}
+
+	println("Msg from server=", string(reply))
+
+	_, err = conn.Write([]byte(strEcho))
+	if err != nil {
+		println("Write to server failed:", err.Error())
+		os.Exit(1)
+	}
+
+	println("write to server = ", strEcho)
+
+	_, err = conn.Read(reply)
+	if err != nil {
+		println("Reply from server failed:", err.Error())
+		os.Exit(1)
+	}
+
+	println("reply from server=", string(reply))
+	conn.Close()
+}
